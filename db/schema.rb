6184353d17bf23b38d8c4bd87b8f7bbd3001d0cd
inpyeong <jinipyung@gmail.com>
2021-06-14 00:23:36 +0900

    Create Bulletin model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_123737) do
+ActiveRecord::Schema.define(version: 2021_06_13_141154) do
+
+  create_table "bulletins", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
