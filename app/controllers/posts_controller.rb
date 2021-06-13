8ca984cbc4055c394e7664b7a1287160fc62f3b3
inpyeong <jinipyung@gmail.com>
2021-06-14 01:06:22 +0900

    Change posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
+  before_action :set_bulletin
   before_action :set_post, only: %i[ show edit update destroy ]
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = @bulletin.present? ? @bulletin.posts.all : Post.all
   end
 
   # GET /posts/1 or /posts/1.json
@@ -12,7 +13,7 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @posts = @bulletin.present? ? @bulletin.posts.new : Post.new
   end
 
   # GET /posts/1/edit
@@ -21,11 +22,11 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = @bulletin.present? ? @bulletin.posts.new(post_params) : Post.new(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { redirect_to (@bulletin.present? ? [@post.bulletin, @post] : @post), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +39,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
+        format.html { redirect_to (@bulletin.present? ? [@post.bulletin, @post] : @post), notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,15 +52,23 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to (@bulletin.present? ? bulletin_posts_url : posts_url), notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
+    def set_bulletin
+      @bulletin = Bulletin.find(params[:bulletin_id]) if params[:bulletin_id].present?
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      if @bulletin.present?
+        @post = @bulletin.posts.find(params[:id])
+      else
+        @post = Post.find(params[:id])
+      end
     end
 
     # Only allow a list of trusted parameters through.
